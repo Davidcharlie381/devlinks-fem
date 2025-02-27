15af4adf688420d63e24de6e1518ed110cd93ba2
Charlie David <user@mx>
2025-02-27 23:43:05 +0100

    add: index

---
+++ app/routes/_protected._index.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button asChild><Link to="/auth">Devlinks</Link></Button>
+    </div>
+  );
+}
